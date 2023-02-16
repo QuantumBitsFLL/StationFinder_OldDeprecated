3ee6676b88ec58b94c2b00e098c2e5ffe10f0321
Quantum Bits <quantum@D304PC11.ichb-g.local>
2023-02-16 15:30:06 +0200

    Initial commit - Alex S. and Ioan C.
    Signed-off-by: Quantum Bits <quantum@D304PC11.ichb-g.local>

---
+++ Styles.css.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+	container: {
+		flex: 1,
+    	backgroundColor: '#20272F',
+		color: '#ddd',
+    	alignItems: 'center',
+    	justifyContent: 'center',
+        lineHeight: 100,
+	},
+  	text: {
+		color: '#ddd',
+        fontSize: 20,
+  	},
+	title: {
+		fontSize: 27,
+		color: "#dff"
+	},
+  	button: {
+		color: "#fff",
+		backgroundColor: "#111a2F",
+		borderColor: "#FFFFFF",
+		borderStyle: "solid",
+        borderWidth: 1,
+		borderRadius: 12,
+		padding: 5,
+        margin: 50,
+        width: '40%',
+        alignContent: 'center',
+        alignItems: 'center',
+  	},
+});
