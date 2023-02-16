e13668f5070e0cd3f6e0132e961fc7c18cdf36fa
Alex Stan <stan.alexandru@ichb.ro>
2023-02-16 16:11:03 +0200

    Added the station screen
    Signed-off-by: Alex Stan <stan.alexandru@ichb.ro>

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import styles from "./Styles.css"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import StationScreen from './StationScreen';
+import styles from './Styles.css';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+        name = 'StationScreen'
+        component = { StationScreen }
+        options = {{ 
+          title: "Station list | StationFinder", 
+          headerShown: false
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
