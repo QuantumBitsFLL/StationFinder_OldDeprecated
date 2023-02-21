d5a41dfee9d885d4054550767b857d0211cba3ae
Alex Stan <stan.alexandru@ichb.ro>
2023-02-21 20:06:02 +0200

    add StationComponent.js
    Signed-off-by: Alex Stan <stan.alexandru@ichb.ro>

---
+++ StationComponent.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { View, Text } from 'react-native'
+
+export const getStations = ( baseUrl ) => {
+    const [ stations, setStations ] = useState( [] ); 
+    useEffect( () => {
+        fetch( `${ baseUrl }/stations`, stations )
+        .then( response => response.json() )  
+        .then( data => setStations( data ) )
+    }, [] );
+}
+
+const Stations = () => {
+    stations = getStations( "localhost:8080" )
+    return ( 
+        <View>
+                { 
+                    stations.map( station => {
+                        <View>
+                            <Text key = { station.id }>{station.name}</Text>
+                        </View>
+                    } 
+                ) }
+        </View>
+    )
+}
+
+export default Stations;
--- StationScreen.js
+++ StationScreen.js
@@ -1,26 +1,12 @@
 import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import Stations from './StationComponent';
 
 const StationScreen = () => {
 
-    // Get the stations from the local json-server
-    const [ stations, setStations ] = useState( [] );
-    useEffect( () => {
-        fetch( 'http://localhost:8080/stations/', stations )
-        .then( response => response.json() )  
-        .then( data => setStations( data ) )
-    }, [] );
-
     return (
         <View>
-            {
-                stations.map( station => (
-                        <View key = { station.id }>
-                            <Text>{ station.name }</Text>
-                        </View>
-                    )
-                )
-            }
+            <Stations/>
         </View>
     )
 }
