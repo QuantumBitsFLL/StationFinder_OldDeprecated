e50e98549d23af838048a37d3a76fdec53fd9d4e
Ioan Cristian Chelaru <ioan.cristian.chelaru@gmail.com>
2023-02-25 17:58:19 +0200

    fixed StationComponent.js
    Signed-off-by: Ioan Cristian Chelaru <ioan.cristian.chelaru@gmail.com>

--- StationComponent.js
+++ StationComponent.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-
+const baseUrl = "http://localhost:8080";
 export const getStations = ( baseUrl ) => {
     const [ stations, setStations ] = useState( [] ); 
     useEffect( () => {
@@ -13,12 +13,19 @@ export const getStations = ( baseUrl ) => {
 }
 
 const Stations = () => {
-    const stations = getStations( "http://localhost:8080" );
+    //const stations = getStations( "http://localhost:8080" );
+    const [ stations, setStations ] = useState( [] ); 
+    useEffect( () => {
+        fetch( `${ baseUrl }/stations`, stations )
+        .then( response => response.json() )  
+        .then( data => setStations( data ) )
+    }, [] );
     return ( 
         <View>
+            {/* <div>{stations.map(s => <div>{s.name}</div>)}</div> */}
                 { 
                     stations.map( station => {
-                        <View>
+                        return <View>
                             <Text key = { station.id }>{ station.name }</Text>
                         </View>
                     } 
