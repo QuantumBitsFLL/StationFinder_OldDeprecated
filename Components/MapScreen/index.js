a303518114e653c8f7e703cec0c0aba60e2a4153
Alex Stan <stan.alexandru@ichb.ro>
2023-02-26 19:37:13 +0200

    added the map screen
    Signed-off-by: Alex Stan <stan.alexandru@ichb.ro>

---
+++ Components/MapScreen/index.js
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+import MapView from "react-native-maps"
+
+const MapScreen = () => {
+    <View>
+        <MapView/>
+    </View>
+}
+
+export default MapScreen;
